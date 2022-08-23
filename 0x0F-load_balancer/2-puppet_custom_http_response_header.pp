9842df195ce3e3bedd429cc5c0d6f84b28a6d761
SFreire2022 <sebastiancazenave@gmail.com>
2022-08-22 23:38:56 -0300

    Fix file_line

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -15,13 +15,6 @@ file_line { 'X-Served-By':
   line   => 'add_header X-Served-By $hostname;',
 }
 
-file_line { 'redirect_me':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
-}
-
 service { 'nginx':
   ensure  => running,
   require => Package['nginx'],
